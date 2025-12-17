539ddd051b62d0f5baea516cbb60abc07a5c7aef
Rythem aggarwal <rythemaggarwal7840@gmail.com>
2025-12-17 12:55:56 +0530

    Fixed config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,4 @@
-// // config.js - HARDCODED for production
-// export const API_URL = 'https://audit-4ury.onrender.com';
-
-// console.log('🔥 API_URL (HARDCODED):', API_URL);
-
-
-// config.js - LOCAL DEVELOPMENT
-export const API_URL = 'http://localhost:3001';
+// config.js - Production
+export const API_URL = 'https://niit-audit-backend.onrender.com';
 
 console.log('🔥 API_URL:', API_URL);
-
-// Production URL (Render)
-export const API_URL = 'https://niit-audit-backend.onrender.com';
